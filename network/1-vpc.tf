a7396162bff6ddefb81048115d26d522208c444d
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2025-11-06 10:15:16 +0100

    🐛 Fix/fix a typeo

--- network/1-vpc.tf
+++ network/1-vpc.tf
@@ -55,35 +55,3 @@ resource "aws_subnet" "private-subnet-B" {
     Name = "${var.SUBNET_NAMES[3]}"
   }
 }
-
-# Custom internet Gateway
-resource "aws_internet_gateway" "demo-gw" {
-  vpc_id = aws_vpc.demo-vpc.id
-
-  tags = {
-    Name = "${var.GW_NAME}"
-  }
-}
-
-#Routing Table for the Custom VPC
-resource "aws_route_table" "demo-rt" {
-  vpc_id = aws_vpc.demo-vpc.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.demo-gw.id
-  }
-
-  tags = {
-    Name = "${var.PUBLIC_RT_NAME}"
-  }
-}
-
-resource "aws_route_table_association" "public-A" {
-  subnet_id      = aws_subnet.public-subnet-A.id
-  route_table_id = aws_route_table.demo-rt.id
-}
-
-resource "aws_route_table_association" "public-B" {
-  subnet_id      = aws_subnet.public-subnet-B.id
-  route_table_id = aws_route_table.demo-rt.id
-}
